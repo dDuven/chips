1c85b73db4179cef32276713458f5f9bc73ddd1e
Devon Edwrds <devonjames.edwards@gmail.com>
2021-04-26 14:17:58 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+let booklist = [
+  {
+    title:'Tightrope-Americans Reaching for Hope',
+    author: 'Nicholas D. Kristof and Sheryl WuDunn',
+    publishedAt: new Date('2020-01-20'),
+  },
+  {
+    title:'Dear Edward',
+    author:'Ann Napolitano',
+    publishedAt: new Date('2019-12-18'),
+  },
+  {
+    title:'I Am the Lorax',
+    author:'Courtney Carbone',
+    publishedAt: new Date('2020-05-17'),
+  },
+  {
+    title:'Devon Autobiography',
+    author:'John Smith',
+    publishedAt: new Date('2021-04-21'),
+  }
+];
+
+
+  
+  res.render('index', {books: booklist});
 });
 
 module.exports = router;
